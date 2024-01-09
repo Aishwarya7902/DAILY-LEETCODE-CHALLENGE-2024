e5f7f6a6773fcf74cb80de2405cac26ffc79d8ce
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-01-09 04:38:48 -0800

    Update 9th Jan__Leaf-Similar Trees.cpp

--- JANUARY/9th Jan__Leaf-Similar Trees.cpp
+++ JANUARY/9th Jan__Leaf-Similar Trees.cpp
@@ -1,5 +1,7 @@
 PROBLEM:https://leetcode.com/problems/leaf-similar-trees/description/?envType=daily-question&envId=2024-01-09
-VIDEO(MIK BHAIYA):
+VIDEO(MIK BHAIYA): https://www.youtube.com/watch?v=rlWJlXQNt0Q
+Problem Name : Leaf-Similar Trees
+Company Tags  : Meta, Microsoft, Twitter, Amazon, Goldman Sachs
 
 /*
   BRUTE FORCE (solved on my own)
@@ -25,6 +27,36 @@ public:
         findLeafNode(v2,root2);
        
     return v1==v2;
+
+    }
+};
+
+/*
+  OPTIMISATION  (USING A STRING)
+  //T.C : O(n)
+//S.C : O(n) (string s) - I am ignoring the recursion stack space here
+*/
+
+
+class Solution {
+public:
+    void findLeafNode(string &s ,TreeNode* root){
+        if(root==NULL)return;
+        if(!root->left && !root->right)
+         {
+             s+=to_string(root->val)+"_";
+         }
+        findLeafNode(s,root->left);
+        findLeafNode(s,root->right);
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        string s1="";
+        string s2="";
+
+        findLeafNode(s1,root1);
+        findLeafNode(s2,root2);
+       
+    return s1==s2;
 
     }
 };
