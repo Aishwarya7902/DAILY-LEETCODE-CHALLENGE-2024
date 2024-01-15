4c0516b15f6fe18e6d8b7a07fbbbbb3a62c022ef
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-01-15 02:34:32 -0800

    Update 15th Jan__ Find Players With Zero or One Losses.cpp

--- JANUARY/15th Jan__ Find Players With Zero or One Losses.cpp
+++ JANUARY/15th Jan__ Find Players With Zero or One Losses.cpp
@@ -1,5 +1,8 @@
 PROBLEM :https://leetcode.com/problems/find-players-with-zero-or-one-losses/description/?envType=daily-question&envId=2024-01-15
-VIDEO (MIK BHAIYA):
+VIDEO (MIK BHAIYA): https://www.youtube.com/watch?v=bXptmD2qWYI
+
+Problem Name : Find Players With Zero or One Losses
+Company Tags  : Google
 
 /*
 
@@ -62,6 +65,56 @@ public:
         answer.push_back(LostExactlyOneMatch);
 
         return answer;
+
+    }
+};
+
+
+/*
+  OPTIMISATION :
+  TC:
+  SC:
+*/
+
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        
+
+      
+         unordered_map<int,int>lost_map;
+        //step 1 ...travel in matches..and keep track of people who lost
+        for(auto &vec:matches){
+            int loser=vec[1];
+            lost_map[loser]++;
+        }
+
+        
+        //take a vector to store people did not lost any match
+        vector<int>neverLost;
+        //take a vector to store people who lost exaclty one match
+
+        vector<int>lostOnce;
+        for(auto &vec:matches){
+            int winner=vec[0];
+            int loser=vec[1];
+            if(lost_map.find(winner)==lost_map.end()){
+                neverLost.push_back(winner);
+                lost_map[winner]=2; //we made it 2 so that same no does not comes twice
+            }
+
+            if(lost_map[loser]==1){
+                lostOnce.push_back(loser);
+                
+            }
+        }
+        
+        
+        sort(begin(neverLost),end(neverLost));
+        sort(begin(lostOnce),end(lostOnce));
+       
+
+        return {neverLost,lostOnce};
 
     }
 };
