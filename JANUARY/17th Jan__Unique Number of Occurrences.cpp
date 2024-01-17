914bad0710c28eb8c6657abd3f8ec95005c51dfb
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-01-17 01:56:36 -0800

    Update 17th Jan__Unique Number of Occurrences.cpp

--- JANUARY/17th Jan__Unique Number of Occurrences.cpp
+++ JANUARY/17th Jan__Unique Number of Occurrences.cpp
@@ -1,5 +1,5 @@
 PROBLEM :https://leetcode.com/problems/unique-number-of-occurrences/?envType=daily-question&envId=2024-01-17
-VIDEO (MIK BHAIYA):
+VIDEO (MIK BHAIYA): https://www.youtube.com/watch?v=b2_rw2NZkr8
 
 
 /*
@@ -28,5 +28,32 @@ public:
            st.insert(freq);
        } 
        return true;
+    }
+};
+
+
+/*
+  OPTIMISATION
+  TC:O(nlogn)
+  SC:o(1)
+  */
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        //if u look at the contraints which is
+        //-1000 <= arr[i] <= 1000 so Making benefit of constraints
+
+        vector<int>vec(2001,0);
+        for(int &x:arr){
+            vec[x+1000]++;
+        }
+
+        sort(begin(vec),end(vec));
+        for(int i=1;i<2001;i++){
+            if(vec[i]!=0 && vec[i]==vec[i-1])return false;
+        }
+
+    return true;
     }
 };
