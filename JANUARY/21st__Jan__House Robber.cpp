bc87be7dc5701491622ab8eae264bb2560d50b07
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-01-21 00:14:11 -0800

    Update 21st__Jan__House Robber.cpp

--- JANUARY/21st__Jan__House Robber.cpp
+++ JANUARY/21st__Jan__House Robber.cpp
@@ -26,3 +26,30 @@ public:
     }
 };
 
+
+/*
+  MEMOIZATION 
+  TC: O(N)
+  SC: O(1) //since constraints are low
+  */
+
+class Solution {
+public:
+    int dp[101];
+    int n;
+    int solve(int indx,vector<int>& nums){
+       if(indx>=n)return 0;
+
+       if(dp[indx]!=-1)return dp[indx];
+
+       int take= nums[indx]+solve(indx+2,nums);
+       int not_take= solve(indx+1,nums);
+       return dp[indx]= max(take,not_take);
+    }
+    int rob(vector<int>& nums) {
+        n=nums.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(0,nums);
+    }
+};
+
