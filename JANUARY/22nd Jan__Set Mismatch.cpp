e4bd60fc8a34dfbf89e690b8abda40b973004d8a
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-01-22 07:41:16 -0800

    Update 22nd Jan__Set Mismatch.cpp

--- JANUARY/22nd Jan__Set Mismatch.cpp
+++ JANUARY/22nd Jan__Set Mismatch.cpp
@@ -1,8 +1,10 @@
 PROBLEM :https://leetcode.com/problems/set-mismatch/description/?envType=daily-question&envId=2024-01-22
-VIDEO (MIK BHAIYA) :
+VIDEO (MIK BHAIYA) : https://www.youtube.com/watch?v=j89Yzq3IwVY
+Problem Name : Set Mismatch (Leetcode 645)
+Company Tags  : Amazon
 
 /*
-  BRUTE FORCE:
+  BRUTE FORCE:(solved on own)
   TC: O(n)
   SC: O(n)
 */
@@ -26,5 +28,33 @@ public:
           }
       }
       return {repeated,notPresent};
+    }
+};
+
+/*
+  OPTIMISATION...watch the video...u will understand better
+  TC:O(N)
+  SC:O(1)
+  */
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n=nums.size();
+        int duplicate=-1;
+        int missing=-1;
+
+        for(int i=0;i<n;i++){
+            if(nums[abs(nums[i])-1]<0)
+             duplicate= abs(nums[i]);
+            else
+             nums[abs(nums[i])-1]*=(-1);
+        }
+
+        for(int i=0;i<n;i++){
+            if(nums[i]>0)
+             missing=(i+1);
+        }
+        return {duplicate,missing};
     }
 };
