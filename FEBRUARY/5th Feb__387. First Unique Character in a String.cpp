6db722bf728ac19efcdbba7503ccb325e0eac31a
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-05 00:38:25 -0800

    Update 5th Feb__387. First Unique Character in a String.cpp

--- FEBRUARY/5th Feb__387. First Unique Character in a String.cpp
+++ FEBRUARY/5th Feb__387. First Unique Character in a String.cpp
@@ -1,6 +1,6 @@
 PROBLEM :https://leetcode.com/problems/first-unique-character-in-a-string/description/?envType=daily-question&envId=2024-02-05
 
-VIDEO :
+VIDEO : https://www.youtube.com/watch?v=vqLiP9cdDLA
 
 
 /*
@@ -24,3 +24,27 @@ public:
         return -1;
     }
 };
+
+
+/*
+SPACE OPTIMIZATION
+  */
+
+//TC :O(n)
+//SC: O(1).....since it contains only a fixed size vector of size 26(only smallcase english alphabet)
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int>mp(26);
+        for(char ch:s){
+            mp[ch-'a']++;
+        }
+
+        for(int i=0;i<s.size();i++){
+            if(mp[s[i]-'a']==1)return i;
+        }
+
+        return -1;
+    }
+};
+
