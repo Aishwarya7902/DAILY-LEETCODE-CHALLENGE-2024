82eada85013a977470d8329accf0158ca4fddcf3
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-06 05:14:15 -0800

    Update 6th Feb__49. Group Anagrams.cpp

--- FEBRUARY/6th Feb__49. Group Anagrams.cpp
+++ FEBRUARY/6th Feb__49. Group Anagrams.cpp
@@ -75,3 +75,47 @@ public:
         return result;
     }
 };
+
+/*
+OPTIMISATION
+T.C : O(n*(k+26)) ....approax= O(n*k)
+(n = size of input, k = maximum length of a string in the input vector)
+S.C : O(n*k)
+*/
+class Solution {
+public:
+    string Generate(string &word){
+        int arr[26]={0};
+        
+        for(char &ch:word){
+            arr[ch-'a']++;
+        }
+       string new_word="";
+        for(int i=0;i<26;i++){
+            int freq=arr[i];
+            if(freq>0){
+               new_word+=string(freq,i+'a');
+            }
+        }
+
+    return new_word;
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
+        int n=strs.size();
+        unordered_map<string,vector<string>>mp;
+
+        for(int i=0;i<n;i++){
+            string word=strs[i];
+            string newWord=Generate(word);
+            mp[newWord].push_back(word);
+        }
+
+        for(auto it:mp){
+            result.push_back(it.second);
+        }
+
+        return result;
+    }
+};
+
