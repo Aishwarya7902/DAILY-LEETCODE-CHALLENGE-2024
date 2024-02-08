3b5dd43674ac95805cc9254bd03b57c73e64f648
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-08 05:25:23 -0800

    Update 8th Feb__Perfect Squares.cpp

--- FEBRUARY/8th Feb__Perfect Squares.cpp
+++ FEBRUARY/8th Feb__Perfect Squares.cpp
@@ -5,3 +5,27 @@ VIDEO :
 https://www.youtube.com/watch?v=Zbn8in7hpXY
 
 */
+  Recursion__ Gives TLE
+  TC:
+  SC:
+
+  */
+class Solution {
+public:
+    int solve(int n){
+      if(n==0)return 0;
+
+      int minCount=INT_MAX;
+      for(int i=1;i*i<=n;i++){
+          int result=1+solve(n-i*i);
+          minCount=min(minCount,result);
+      } 
+      return minCount; 
+    }
+
+    int numSquares(int n) {
+        return solve(n);
+
+    }
+};
+    
