a71b0c4b28b330dfa0f1a9fbd7a836b6cb09a16d
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-09 08:33:10 -0800

    Update 9th Feb__368. Largest Divisible Subset.cpp

--- FEBRUARY/9th Feb__368. Largest Divisible Subset.cpp
+++ FEBRUARY/9th Feb__368. Largest Divisible Subset.cpp
@@ -1,6 +1,7 @@
 PROBLEM:https://leetcode.com/problems/largest-divisible-subset/description/?envType=daily-question&envId=2024-02-09
 VIDEO : https://www.youtube.com/watch?v=T-Fk9185a68
-
+Problem Name : Largest Divisible Subset
+Company Tags  : Google
 /*
   BRUTE FORCE (recursion)
   TC:O(2^n)
@@ -35,6 +36,52 @@ public:
         vector<int>temp;
         int prev=-1;
         solve(0,nums,result,temp,prev);
+
+        return result;
+
+    }
+};
+
+
+/*
+BOTTOM UP
+TC:O(N^2)
+SC:
+*/
+
+
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int n=nums.size();
+        sort(begin(nums),end(nums));//most crucial step
+        vector<int> dp(n,1);
+        vector<int> prev_indx(n,-1);
+
+        int last_chosen_indx=0;
+        int maxL=1;
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]%nums[j]==0){
+                    if(dp[i]<dp[j]+1){
+                        dp[i]=dp[j]+1;
+                        prev_indx[i]=j;
+                    }
+
+                    if(dp[i]>maxL){
+                        maxL=dp[i];
+                        last_chosen_indx=i;
+                    }
+                }
+            }
+        }
+
+        vector<int>result;
+        while(last_chosen_indx!=-1){
+            result.push_back(nums[last_chosen_indx]);
+            last_chosen_indx=prev_indx[last_chosen_indx];
+        }
 
         return result;
 
