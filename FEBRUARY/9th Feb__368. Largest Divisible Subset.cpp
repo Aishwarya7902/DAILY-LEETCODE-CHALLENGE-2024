e86827d99d42762bb1361448ffb5d11c03268220
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-09 07:45:56 -0800

    Update 9th Feb__368. Largest Divisible Subset.cpp

--- FEBRUARY/9th Feb__368. Largest Divisible Subset.cpp
+++ FEBRUARY/9th Feb__368. Largest Divisible Subset.cpp
@@ -8,4 +8,35 @@ VIDEO : https://www.youtube.com/watch?v=T-Fk9185a68
 
 */
 
+class Solution {
+public:
 
+    void solve(int indx,vector<int>& nums,vector<int>& result,vector<int>& temp,int prev){
+        if(indx>=nums.size()){
+            if(temp.size()>result.size()){
+                result=temp;
+            }
+            return;
+        }
+
+        //taken 
+        if(prev==-1 || nums[indx]%prev==0){
+            temp.push_back(nums[indx]);
+            solve(indx+1,nums,result,temp,nums[indx]);
+            temp.pop_back();//not taken
+        }
+        //not taken
+       solve(indx+1,nums,result,temp,prev); 
+    }
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(begin(nums),end(nums));//most crucial step
+
+        vector<int>result;
+        vector<int>temp;
+        int prev=-1;
+        solve(0,nums,result,temp,prev);
+
+        return result;
+
+    }
+};
