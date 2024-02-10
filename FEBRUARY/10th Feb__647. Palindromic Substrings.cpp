0fa1a73b2b99975ca72c82a306b9b575cb9dcfd9
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-10 04:18:31 -0800

    Update 10th Feb__647. Palindromic Substrings.cpp

--- FEBRUARY/10th Feb__647. Palindromic Substrings.cpp
+++ FEBRUARY/10th Feb__647. Palindromic Substrings.cpp
@@ -4,7 +4,7 @@ VIDEO :
 
 /*
   BRUTE FORCE (SOLVED ON MY OWN 😎)
-  TC: O(N^2)
+  TC: O(N^3)
   SC: O(1)
 
   */
@@ -37,3 +37,33 @@ public:
 };
 
 
+/*
+METHOD 2
+USING RECURSION
+TC:
+SC:
+  */
+
+class Solution {
+public:
+     bool isPalindrome(int i,int j,string s){
+        if(i>j)return true;
+
+        if(s[i]==s[j])return isPalindrome(i+1,j-1,s);
+
+        return false;
+    }
+    int countSubstrings(string s) {
+        int cnt=0;
+        int n=s.size();
+
+        for(int i=0;i<n;i++){
+            string curr=" ";
+            for(int j=i;j<n;j++){
+               
+                if(isPalindrome(i,j,s))cnt++;
+            }
+        }
+        return cnt;
+    }
+};
