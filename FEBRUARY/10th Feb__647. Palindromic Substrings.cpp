21922548b8298e237eb582f16daed642f2c2c411
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-10 07:50:56 -0800

    Update 10th Feb__647. Palindromic Substrings.cpp

--- FEBRUARY/10th Feb__647. Palindromic Substrings.cpp
+++ FEBRUARY/10th Feb__647. Palindromic Substrings.cpp
@@ -101,3 +101,43 @@ public:
         return cnt;
     }
 };
+
+
+/*
+BOTTOM UP
+TC:O(N^2)
+SC:O(N^2)
+
+  */
+
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n=s.size();
+        vector<vector<bool>>dp(n,vector<bool>(n,false));
+        int count=0;
+        //dp[i][j]=true ....means substring between i to j is a palindrome
+
+        for(int L=1;L<=n;L++){
+          for(int i=0;i+L-1<n;i++){
+              int j=i+L-1;
+              if(i==j){ //length 1
+                dp[i][j]=true;
+              }
+
+              else if(i+1==j){ //length 2
+
+                dp[i][j]=(s[i]==s[j]);
+              }
+
+              else{
+                  
+                  dp[i][j]=(s[i]==s[j] && dp[i+1][j-1]);
+              }
+
+              if(dp[i][j]==true)count++;
+          }
+        }
+        return count;
+    }
+};
