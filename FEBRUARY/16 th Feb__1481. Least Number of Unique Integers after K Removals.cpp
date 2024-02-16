264a0d2d64457ec50ae6d0e1f7724ec1e7b0694f
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-15 23:55:39 -0800

    Create 16 th Feb__1481. Least Number of Unique Integers after K Removals.cpp

---
+++ FEBRUARY/16 th Feb__1481. Least Number of Unique Integers after K Removals.cpp
@@ -0,0 +1,63 @@
+PROBLEM :https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/description/?envType=daily-question&envId=2024-02-16
+VIDEO:
+
+/*
+0 - 6 months
+JPMorgan
+15
+6 months - 1 year
+Amazon
+2
+1 year - 2 years
+Booking.com
+4
+Expedia
+2
+Dosh
+*/
+
+
+/*
+  BRUTE FORCE (SOLVED ON MY OWN)
+  ALGO:
+  1. Use a map to count the frequencies of the numbers in the array.
+  2. An optimal strategy is to remove the numbers with the smallest count first.
+
+  */
+
+class Solution {
+public:
+    typedef pair<int,int>P;
+    static bool comp(P &a,P &b){
+        return a.second>b.second;
+    }
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+      unordered_map<int,int>mp; //SC:O(n)
+      vector<P>vec; //SC:O(n)
+      for(auto it:arr){ //TC:O(n)
+          mp[it]++;
+      }
+
+      for(auto it:mp){ //TC:O(n)
+          int num=it.first;
+          int freq=it.second;
+
+          vec.push_back({num,freq});
+      }
+
+      sort(begin(vec),end(vec),comp); //TC:O(nlogn)
+      
+      while(k>0){ //TC:O(k)
+        int i=vec.size()-1;
+        if(k>=vec[i].second){
+            k-=vec[i].second;
+            vec.pop_back();
+        }
+       else{
+           break;
+       }
+      }
+
+      return vec.size();
+    }
+};
