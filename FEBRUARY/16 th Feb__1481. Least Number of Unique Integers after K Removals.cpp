0ce137041a62d36769b7eb772bb12644bae6fdab
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-16 04:54:35 -0800

    Update 16 th Feb__1481. Least Number of Unique Integers after K Removals.cpp

--- FEBRUARY/16 th Feb__1481. Least Number of Unique Integers after K Removals.cpp
+++ FEBRUARY/16 th Feb__1481. Least Number of Unique Integers after K Removals.cpp
@@ -1,5 +1,5 @@
 PROBLEM :https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/description/?envType=daily-question&envId=2024-02-16
-VIDEO:
+VIDEO:https://www.youtube.com/watch?v=O5v9Tqv2ZDA
 
 /*
 0 - 6 months
@@ -82,7 +82,7 @@ public:
           mp[it]++;
       }
 
-      for(auto it:mp){ //TC:O(n)
+      for(auto it:mp){ //TC:O(nlogn)
           int num=it.first;
           int freq=it.second;
 
@@ -141,5 +141,45 @@ public:
         }
 
      return 0 ;// no elements were left ...all deleted
+    }
+};
+
+
+/*
+  BEST OPTIMIZATION
+  TC:O(N)
+  SC:O(N)
+*/
+
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        int n=arr.size();
+        unordered_map<int,int>mp;
+
+        for(int &x:arr){
+            mp[x]++;
+        }
+
+        vector<int>freqCount(n+1);
+        //freqCount[i]= no of elements(type) having freq i
+
+        int uniqueElement=mp.size();
+
+        for(auto it:mp){
+            int freq=it.second;
+            freqCount[freq]++;
+        }
+
+       for(int freq=1;freq<=n;freq++){
+           int typeICanDelete=min(k/freq,freqCount[freq]);
+           k-=typeICanDelete*freq;
+           uniqueElement-=typeICanDelete;
+
+           if(k<=freq)return uniqueElement;
+       }
+
+       return 0;
+       
     }
 };
