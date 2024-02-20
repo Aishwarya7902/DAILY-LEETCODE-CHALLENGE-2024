84dc9cdcc02276fa854e45e72bfb80ca5b1dfd8d
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-19 23:04:46 -0800

    Create 20th Feb__268. Missing Number.cpp

---
+++ FEBRUARY/20th Feb__268. Missing Number.cpp
@@ -0,0 +1,57 @@
+PROBLEM :https://leetcode.com/problems/missing-number/description/?envType=daily-question&envId=2024-02-20
+
+VIDEO : 
+/*
+Companies
+0 - 6 months
+Amazon
+4
+Adobe
+4
+Microsoft
+2
+Google
+2
+Apple
+2
+Nvidia
+2
+warnermedia
+2
+6 months - 1 year
+Bloomberg
+2
+Uber
+2
+1 year - 2 years
+Facebook
+5
+Yahoo
+2
+Oracle
+2
+VMware
+2
+Snapchat
+2
+*/
+
+
+/*
+  BRUTE FORCE 
+  USING SORTING
+  */
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        sort(begin(nums),end(nums)); //TC: O(nlogn)
+
+        for(int i=0;i<n;i++){ // TC:O(n)
+            if(i!=nums[i])return i;
+        }
+
+        return n;
+    }
+};
