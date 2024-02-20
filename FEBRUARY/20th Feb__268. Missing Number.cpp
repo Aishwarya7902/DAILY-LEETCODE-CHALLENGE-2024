c558acdebc04f56eb4b8a7419a1ddcac7202361a
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-20 00:14:43 -0800

    Update 20th Feb__268. Missing Number.cpp

--- FEBRUARY/20th Feb__268. Missing Number.cpp
+++ FEBRUARY/20th Feb__268. Missing Number.cpp
@@ -127,3 +127,32 @@ public:
         return result;
     }
 };
+
+/*
+APPROACH 5
+  BINARY SEARCH
+  TC: O(nlogn)
+  SC: O(1)
+  */
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        sort(begin(nums),end(nums));
+        int result=n;
+
+        int l=0,r=n-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>mid){
+                result=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return result;
+    }
+};
