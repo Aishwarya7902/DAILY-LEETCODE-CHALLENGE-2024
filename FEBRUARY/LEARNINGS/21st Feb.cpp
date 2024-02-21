c1140d275ec68760d6a6f0069e1fc591d70fd637
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-02-21 06:29:18 -0800

    Create 21st Feb.cpp

---
+++ FEBRUARY/LEARNINGS/21st Feb.cpp
@@ -0,0 +1,6 @@
+USE CASE OF n & (n-1)
+
+  1. clean the right most set bit.
+  2.helps to find the no of set bit in a number.......... do n&(n-1)   untill all bits become zero
+  3. check power of 2
+    return n>0 && (n&(n-1));
