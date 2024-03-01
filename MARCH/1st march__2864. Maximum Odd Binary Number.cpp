9bffe1716be826e3a0813965750fe0b269cf4bb5
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-03-01 01:55:38 -0800

    Update 1st march__2864. Maximum Odd Binary Number.cpp

--- MARCH/1st march__2864. Maximum Odd Binary Number.cpp
+++ MARCH/1st march__2864. Maximum Odd Binary Number.cpp
@@ -1,5 +1,5 @@
 PROBLEM:https://leetcode.com/problems/maximum-odd-binary-number/description/?envType=daily-question&envId=2024-03-01
-VIDEO:
+VIDEO: https://www.youtube.com/watch?v=IRO_4X0csoc
 
 
 
@@ -10,3 +10,28 @@ VIDEO:
   The binary representation of an odd number contains '1' in the least significant place.
   */
 
+/*
+  TC:O(n)
+  SC:O(N)
+*/
+
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int n=s.size();
+        string result=string(n,'0');
+        int i=0;
+        for(char &ch:s){
+            if(ch=='1'){
+                if(result[n-1]=='1'){//The binary representation of an odd number contains '1' in the least significant place.
+                   result[i]='1';
+                   i++;
+                }
+                else{
+                    result[n-1]='1';
+                }
+            }
+        }
+        return result;
+    }
+};
