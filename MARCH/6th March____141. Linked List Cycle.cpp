e26383105596c3786deb1e6b61c942b2f9539906
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-03-06 05:59:20 -0800

    Update 6th March____141. Linked List Cycle.cpp

--- MARCH/6th March____141. Linked List Cycle.cpp
+++ MARCH/6th March____141. Linked List Cycle.cpp
@@ -68,3 +68,37 @@ public:
         return false;
     }
 };
+
+
+/*
+APPOACH 2
+TC: O(N)
+SC:O(1)
+  */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next)return false;
+
+        
+        ListNode * slow=head;
+        ListNode * fast=head;
+        while(fast && fast->next){
+             
+             slow=slow->next;
+             fast=fast->next->next;
+             if(fast==slow)return true;
+        }
+
+        return false;
+    }
+};
