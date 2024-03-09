18ac42935d7af407a315d11dc8663d6d9f45a9a4
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-03-08 23:52:33 -0800

    Update 7th  march__876. Middle of the Linked List.cpp

--- MARCH/7th  march__876. Middle of the Linked List.cpp
+++ MARCH/7th  march__876. Middle of the Linked List.cpp
@@ -84,3 +84,25 @@ public:
       return slow;
     }
 };
+
+
+/*
+APPROACH 2:
+TC:O(M+N)
+SC:O(1)
+    */
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int m=nums1.size();
+        int n=nums2.size();
+        int i=0,j=0;
+        while(i<m && j<n){
+            if(nums1[i]==nums2[j])return nums1[i];
+
+            else if(nums1[i]<nums2[j])i++;
+            else j++;
+        }
+        return -1;
+    }
+};
