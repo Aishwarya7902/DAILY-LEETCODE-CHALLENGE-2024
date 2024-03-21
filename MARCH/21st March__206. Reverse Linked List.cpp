e7a224ae5a1c305936f39cd121caf5cff208adc1
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-03-21 00:55:41 -0700

    Create 21st March__206. Reverse Linked List.cpp

---
+++ MARCH/21st March__206. Reverse Linked List.cpp
@@ -0,0 +1,27 @@
+PROBLEM :https://leetcode.com/problems/reverse-linked-list/description/?envType=daily-question&envId=2024-03-21
+
+VIDEO :
+
+/*
+  BRUTE FORCE (OWN)
+  TC:O(N)
+  SC:O(1)
+  */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next)return head;
+        ListNode* temp=head->next;
+        head->next=NULL;
+        ListNode* ans=reverseList(temp); //recursion will do rest 
+        // result will be  5->4->3->2 (eg 1)
+
+        // now just put head in the end.. 5->4->3->2->1
+
+        temp->next=head;
+        return ans;
+
+
+    }
+};
