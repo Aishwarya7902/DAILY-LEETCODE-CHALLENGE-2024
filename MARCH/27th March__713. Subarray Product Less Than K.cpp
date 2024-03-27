4a1f7f9fee6475cfe6903f97f6f2780b9d7e9a33
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-03-27 06:28:57 -0700

    Update 27th March__713. Subarray Product Less Than K.cpp

--- MARCH/27th March__713. Subarray Product Less Than K.cpp
+++ MARCH/27th March__713. Subarray Product Less Than K.cpp
@@ -1,6 +1,8 @@
 PROBLEM :https://leetcode.com/problems/subarray-product-less-than-k/description/?envType=daily-question&envId=2024-03-27
 
-VIDEO :
+VIDEO :https://www.youtube.com/watch?v=9fmKB1F1pEE
+Problem Name : Subarray Product Less Than K
+Company Tags  : Yatra
 
 /*
   BRUTE FORCE
@@ -25,6 +27,44 @@ public:
                 if(prod<k)count++;
                 else break;
             }
+        }
+
+        return count;
+    }
+};
+
+
+/*
+SLIDING WINDOW APPROACH
+TC:O(N)
+SC:O(1)
+*/
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int n=nums.size();
+     /*
+      IMPORTANT CORNER CASE
+      */
+
+      if(k<=1)return 0;// since no. are always >=1 , 
+      //hence it's never possible to find a subarray with prod<1
+        int count=0;
+        int i=0;
+        int j=0;
+        int prod=1;
+        while(j<n){
+            prod*=nums[j];
+            while(prod>=k){ //untill condition violates remove ith index
+             prod/=nums[i];
+             i++;
+
+            }
+
+            // now condition is valid
+            count+=j-i+1;//no. of subarrays ending at j
+            j++;
         }
 
         return count;
